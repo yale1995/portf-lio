aef0bbd963aba76b7733ab411a5624e1534585c1
Yale Araujo <yale850@gmail.com>
2022-04-07 15:35:08 -0300

    new version

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,16 +3,25 @@ import styles from "./styles.module.scss";
 import { ActiveLink } from "../ActiveLink";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { HiMenu } from "react-icons/hi";
 import Link from "next/link";
+import { useState } from "react";
 
 export function Header() {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <Link href='/' prefetch>
+        <Link href="/" prefetch>
           <img src="/images/yale.svg" alt="Logotipo" />
         </Link>
-        <nav>
+        <div className={styles.menuIcon} onClick={() => setMenuIsOpen(!menuIsOpen)}>
+          <HiMenu className={styles.icon} />
+        </div>
+        <nav
+          className={menuIsOpen ? styles.menu : ""}
+        >
           <ActiveLink activeClassName={styles.active} href="/" prefetch>
             <a>Home</a>
           </ActiveLink>
